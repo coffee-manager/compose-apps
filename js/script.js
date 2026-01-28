1d6cbe3edd52a16eda0145b33c6259e65bf276a9
JiHoon <kjhktc@naver.com>
2026-01-28 21:55:52 +0900

    첫커밋

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * 공통 HTML(header/footer)을 fetch로 불러온다
+ */
+function loadCommon() {
+    fetch('/header.html')
+        .then(r => r.text())
+        .then(html => document.getElementById('header').innerHTML = html);
+
+    fetch('/footer.html')
+        .then(r => r.text())
+        .then(html => document.getElementById('footer').innerHTML = html);
+}
+
+loadCommon();
