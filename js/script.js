0e346ca41ff75938d7411beb9fb3ec6621b12111
JiHoon <kjhktc@naver.com>
2026-02-01 19:26:53 +0900

    footer 제거 header 에 개발자 문의 추가

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 /**
- * 공통 HTML(header/footer)을 fetch로 불러온다
+ * 공통 HTML header 를 fetch로 불러온다
  */
 function loadCommon() {
     fetch('header.html')
         .then(r => r.text())
         .then(html => document.getElementById('header').innerHTML = html);
-
-    fetch('footer.html?v=20260131')
-        .then(r => r.text())
-        .then(html => document.getElementById('footer').innerHTML = html);
 }
 
 loadCommon();
